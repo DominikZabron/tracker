60f4b97cf042890a38f84431d19e1a1a19e97550
DominikZabron <dominik.zabron@gmail.com>
2016-07-28 16:01:49 +0200

    Return correct response.

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,3 +13,8 @@ def app():
 def test_post_item_success(client):
     resp = client.post('/item')
     assert resp.status == falcon.HTTP_201
+
+
+def test_post_item_returns_correct_response(client):
+    resp = client.post('/item')
+    assert 'cart_id' in resp.body
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import
 
 import falcon
+import uuid
+import json
 
 
 class CartItem(object):
     def on_post(self, req, resp):
+        cart_id = uuid.uuid4()
+
         resp.status = falcon.HTTP_201
+        resp.body = json.dumps({'cart_id': str(cart_id)})
 
 app = falcon.API()
 
